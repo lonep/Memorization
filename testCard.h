f45b36efa51cc8e051cf26edb67096954dee231d
lonep <djoefrit@gmail.com>
2020-05-22 22:35:55 +0300

    Create testCard.h

---
+++ testCard.h
@@ -0,0 +1,20 @@
+//
+// Created by lonep on 22-May-20.
+//
+
+#ifndef UNTITLED1_TESTCARD_H
+#define UNTITLED1_TESTCARD_H
+
+
+#include <string>
+#include <vector>
+
+class testCard {
+    std::string question;
+    std::string trueAn;
+    std::vector <std::string> falseAn;
+    short mistakes;
+};
+
+
+#endif //UNTITLED1_TESTCARD_H

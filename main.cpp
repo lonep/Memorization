cd64aa3e8c7366a16c73b2af6561c90ee057150a
lonep <djoefrit@gmail.com>
2020-05-28 12:19:48 +0300

    Clean main.cpp

--- main.cpp
+++ main.cpp
@@ -2,10 +2,5 @@
 #include <fstream>
 #include "txtParser.h"
 int main() {
-    std::ifstream obj("test.txt");
-    bool p = obj.is_open();
-    txtParser lol;
-    lol.parseFile(obj);
-    obj.close();
-    return 0;
+
 }

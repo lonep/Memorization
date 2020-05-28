4a4ff214a37d29c428617c3402a9cc3e468c65db
fenryux <55600745+fenryux@users.noreply.github.com>
2020-05-28 19:37:18 +0300

    writer

--- writerTXT.cpp
+++ writerTXT.cpp
@@ -3,3 +3,28 @@
 //
 
 #include "writerTXT.h"
+
+bool writerTXT::isPATHCorrect(std::string path) {
+    std::ifstream input(path);
+    if(input.is_open()){
+        input.close();
+        return true;
+    }
+    else return false;
+}
+
+void writerTXT::setPATH(std::string str){
+    if(writerTXT::isPATHCorrect(str))
+        PATH = str;
+}
+
+void writerTXT::write() {
+    std::string temp;
+    std::ifstream input(PATH);
+    while(!input.eof()){
+        std::getline(input,temp);
+        if((temp[0]+temp[1]) == "\\q") {
+            card.question = temp;
+        }
+    }
+}

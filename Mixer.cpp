5bc054d46574b3bd950eb9402c69c623b77d840e
Sershhka <57639730+Sershhka@users.noreply.github.com>
2020-06-10 12:33:03 +0400

    fix method Mixer

--- Mixer.cpp
+++ Mixer.cpp
@@ -1,11 +1,13 @@
 #include"Mixer.h"
+#include <ctime>
 
 void Mixer::mixVector(std::vector<std::string> &answers)
 {
 	std::vector<std::string> temp;
 	while (answers.size() != 0)
 	{
-		int rnd = rand() % answers.size()-1;
+		srand(time(0));
+		int rnd = rand() % answers.size();
 		temp.push_back(answers[rnd]);
 		answers.erase(answers.begin() + rnd);
 	}
--- Mixer.h
+++ Mixer.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <vector>
 #include <string>
+
 class Mixer {
 public:
 	void static mixVector(std::vector<std::string> &answers);

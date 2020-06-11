c2fb9861e309dfc2d185c86c37e80ffb280514a4
lonep <djoefrit@gmail.com>
2020-06-11 10:27:04 +0300

    Build #1

--- main.cpp
+++ main.cpp
@@ -11,20 +11,26 @@ int main() {
     setlocale(LC_ALL, "RUS");
     IO_Module UI;
     std::string user_request;
+
     bool WR_status = true;
     UI.Greeting();
-    do{
+    user_request = UI.Menu();
+   /* do{
         user_request = UI.get_PATH();
-    } while (!file::isPATHCorrect(user_request));
+    } while (!file::isPATHCorrect(user_request)); */
     file File(user_request,WR_status);
     std::list<testCard> cards;
     cards = File.read();
     TestManager tm(cards);
+
     auto it = cards.begin();
     while(it != cards.end()){
         UI.show_card(it->get_question(),it->get_all_answers());
-        it++;
+        if(it->check_answer(UI.get_answer(it->get_all_answers()))){
+            it++;
+        }
     }
+    ;
 
 
 }
